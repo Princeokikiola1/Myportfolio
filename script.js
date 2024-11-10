c04327de79565d0d00120926cdd989f6997682bb
Princeokikiola1 <167760808+Princeokikiola1@users.noreply.github.com>
2024-11-10 10:15:42 +0100

    Processing

---
+++ script.js
@@ -0,0 +1,15 @@
+AOS.init();
+
+const app = () => {
+    const isDark = window.matchMedia("(prefers-color-scheme:dark)").matches
+
+  const lightTheme = {
+    backgroundColor : "white",
+    color : "black"
+  }
+
+  const darkTheme = {
+    backgroundColor : "black",
+    color : "white"
+  }
+}
